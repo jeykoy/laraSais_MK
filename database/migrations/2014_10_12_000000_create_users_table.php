e339ed9e9ed664e43d70539c5705edb6e10ea488
James Kevin Avena <jk_avena@umindanao.edu.ph>
2019-09-24 14:40:08 +0800

    created user table, items table, stockins table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,9 @@ class CreateUsersTable extends Migration
             $table->bigIncrements('id');
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('contactNumber');
+            $table->string('address');
+            $table->enum('role',['admin','customer']);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
---
+++ database/migrations/2019_09_24_062041_create_stockins_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStockinsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stockins', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedInteger('itemId');
+            $table->unsignedInteger('quantityAdded');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stockins');
+    }
+}
